98de937ae1e7f20db6f0fc28b0134de6c8c7311a
Darren Burns <darrenb900@gmail.com>
2026-01-21 22:20:22 +0000

    Tabs docs

---
+++ docs/minimal-examples/tabs-basic/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	t "terma"
+)
+
+type App struct {
+	tabState *t.TabState
+}
+
+func NewApp() *App {
+	return &App{
+		tabState: t.NewTabState([]t.Tab{
+			{Key: "home", Label: "Home"},
+			{Key: "settings", Label: "Settings"},
+			{Key: "help", Label: "Help"},
+		}),
+	}
+}
+
+func (a *App) Build(ctx t.BuildContext) t.Widget {
+	return t.Column{
+		Children: []t.Widget{
+			t.TabBar{
+				ID:    "tabs",
+				State: a.tabState,
+			},
+			t.Text{Content: "Active: " + a.tabState.ActiveKey()},
+		},
+	}
+}
+
+func main() {
+	if err := t.Run(NewApp()); err != nil {
+		log.Fatal(err)
+	}
+}
